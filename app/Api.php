58878e1364e2c7889f3396e672f5db67f8ab066d
JAVI-CC <javileg98@hotmail.com>
2020-08-16 19:11:25 +0200

    heroku

--- app/Api.php
+++ app/Api.php
@@ -3,10 +3,23 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Validator;
 
 class Api extends Model
 {
     public $timestamps = false;
     protected $table = 'juegos';
     protected $fillable = array('nombre', 'descripcion', 'desarrolladora', 'fecha');
+
+    public function validation($request) {
+
+        $validator = Validator::make($request->all(), [
+            'nombre' => 'required|min:2|max:255',
+            'descripcion' => 'required|min:10|max:255',
+            'desarrolladora' => 'required|min:2|max:255',
+            'fecha' => 'required',
+        ]);   
+
+        return $validator; 
+    }
 }
--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -7,30 +7,57 @@ use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    public function getAll(){
+
+    protected $api;
+
+    public function __construct(Api $api)
+    {
+        $this->api = $api;
+    }
+
+    public function getAll()
+    {
         $juegos = Api::orderBy('id', 'DESC')->get();
         return $juegos;
-      }
-  
-      public function add(Request $request){
-          $juego = Api::create($request->all());
-          return $juego;
-      }
-  
-      public function get($id) {
-          $juego = Api::find($id);
-          return $juego;
-      }
-  
-      public function edit($id, Request $request) {
-          $juego = $this->get($id);
-          $juego->fill($request->all())->save();
-          return $juego;
-      }
-  
-      public function delete($id) {
-          $juego = $this->get($id);
-          $juego->delete();
-          return $juego;
-      }
+    }
+
+    public function add(Request $request)
+    {
+
+        $validator = $this->api->validation($request);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        } else {
+            $juego = Api::create($request->all());
+            return $juego;
+        }
+    }
+
+    public function get($id)
+    {
+        $juego = Api::find($id);
+        return $juego;
+    }
+
+    public function edit($id, Request $request)
+    {
+
+        $validator = $this->api->validation($request);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        } else {
+            $juego = $this->get($id);
+            $juego->fill($request->all())->save();
+            return $juego;
+        }
+    }
+
+    public function delete($id)
+    {
+        $juego = $this->get($id);
+        $juego->delete();
+        return $juego;
+    }
 }
