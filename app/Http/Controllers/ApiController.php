f7d0ff758d1000634729f863d33d9428ea296d98
JAVI-CC <javileg98@hotmail.com>
2020-08-16 16:01:42 +0200

    heroku

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,12 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Api;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class ApiController extends Controller
 {
-    public function getAll() {
-        $juegos = Api::orderBy('id', 'DESC')->select(DB::raw('md5(id) AS uid'), 'nombre', 'descripcion', 'desarrolladora', 'fecha')->get();
+    public function getAll(){
+        $juegos = Api::orderBy('id', 'DESC')->get();
         return $juegos;
       }
   
@@ -19,17 +18,19 @@ class ApiController extends Controller
       }
   
       public function get($id) {
-          $juego = Api::where(DB::raw('md5(id)'), $id)->select(DB::raw('md5(id) AS uid'), 'nombre', 'descripcion', 'desarrolladora', 'fecha')->first();
+          $juego = Api::find($id);
           return $juego;
       }
   
       public function edit($id, Request $request) {
-          $juego = Api::where(DB::raw('md5(id)'), $id)->update($request->all());
+          $juego = $this->get($id);
+          $juego->fill($request->all())->save();
           return $juego;
       }
   
       public function delete($id) {
-          $juego = Api::where(DB::raw('md5(id)'), $id)->delete();
+          $juego = $this->get($id);
+          $juego->delete();
           return $juego;
       }
 }
