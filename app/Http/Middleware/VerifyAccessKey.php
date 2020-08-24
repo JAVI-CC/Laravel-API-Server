cc0dbe7d69b8c9e0cac31b0e68b46939fefc8da1
JAVI-CC <javileg98@hotmail.com>
2020-08-24 11:55:07 +0200

    heroku

--- app/Http/Middleware/VerifyAccessKey.php
+++ app/Http/Middleware/VerifyAccessKey.php
@@ -19,6 +19,7 @@ class VerifyAccessKey
         $key = $request->headers->get('api_key');
         // Si coincide con el valor almacenado en la aplicacion
         // la aplicacion se sigue ejecutando
+        return $key;
         if ($key == env('API_KEY')) {
             return $next($request);
         } else {
