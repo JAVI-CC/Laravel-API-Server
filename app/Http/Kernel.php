a8ad84b00b1337e2b8ae426f8844e77bbf3b1eba
JAVI-CC <javileg98@hotmail.com>
2020-08-24 11:39:46 +0200

    heroku

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,7 +40,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\AuthKey::class,
+            \App\Http\Middleware\VerifyAccessKey::class,
         ],
     ];
 
---
+++ app/Http/Middleware/VerifyAccessKey.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class VerifyAccessKey
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        // Obtenemos el api-key que el usuario envia
+        $key = $request->headers->get('api_key');
+        // Si coincide con el valor almacenado en la aplicacion
+        // la aplicacion se sigue ejecutando
+        if ($key == env('API_KEY')) {
+            return $next($request);
+        } else {
+            // Si falla devolvemos el mensaje de error
+            return response()->json(['error' => 'unauthorized'], 401);
+        }
+    }
+}
