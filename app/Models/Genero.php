11e930426e5166d4ff9f1cea7afabd76256e5291
JAVI-CC <javileg98@hotmail.com>
2021-05-18 20:47:14 +0200

    add routes generes

--- app/Models/Genero.php
+++ app/Models/Genero.php
@@ -4,6 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @OA\Schema(
+ *   @OA\Xml(name="Genero"),
+ *   @OA\Property(property="nombre", description="Nombre del genero", type="string", example="Rpg de Acción"),
+ *   @OA\Property(property="slug", type="string", description="Url amigable del nombre del genero", example="rpg-de-accion")
+ * )
+ **/
 class Genero extends Base
 {
     //Relacion de muchos a muchos polimorfica
