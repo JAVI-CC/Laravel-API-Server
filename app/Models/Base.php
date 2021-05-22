b0aa6735a548db47e85d6024370d2e24b477e2f3
JAVI-CC <javileg98@hotmail.com>
2021-05-22 20:49:18 +0200

    fix and update postman collection

--- app/Models/Base.php
+++ app/Models/Base.php
@@ -3,16 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Base extends Model
 {
     public function sluggable($string)
     {
-        $slug = substr($string, 0, 140);
-        $slug = strtr($slug, " _ÀÁÂÃÄÅÆàáâãäåæÒÓÔÕÕÖØòóôõöøÈÉÊËèéêëðÇçÐÌÍÎÏìíîïÙÚÛÜùúûüÑñÞßÿý",  "--aaaaaaaaaaaaaaoooooooooooooeeeeeeeeecceiiiiiiiiuuuuuuuunntsyy");
-        $slug = strtolower($slug);
-        $slug = preg_replace("/[^a-z0-9\-.]/", "", $slug);
-        return str_replace("--", "-", $slug);
+        return Str::slug($string);
     }
 
     public function findById($id) {
