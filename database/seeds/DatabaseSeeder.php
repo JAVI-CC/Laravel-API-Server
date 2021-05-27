44bcfc42a1daa14d176a531401cb586b69b480a8
JAVI-CC <javileg98@hotmail.com>
2021-05-27 20:21:30 +0200

    finally

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\DB;
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Seed the applications database.
      *
      * @return void
      */
